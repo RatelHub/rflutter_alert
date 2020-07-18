76072e13b928544c41dee95d15f91d6faadc0c67
onguc <bilirmuhan@gmail.com>
2020-07-18 23:14:06 +0300

    Revert "When we click on the top and bottom of the popup, the popup was closed. But when we click on the space on the rigth and left sides, the popup was not closed. This problem has been fixed with this update"
    This reverts commit 2464e493

--- lib/src/alert.dart
+++ lib/src/alert.dart
@@ -75,52 +75,54 @@ class Alert {
       child: ConstrainedBox(
         constraints: style.constraints ?? BoxConstraints.expand(width: double.infinity, height: double.infinity),
         child: Center(
-          child: AlertDialog(
-            backgroundColor: style.backgroundColor ?? Theme.of(context).dialogBackgroundColor,
-            shape: style.alertBorder ?? _defaultShape(),
-            titlePadding: EdgeInsets.all(0.0),
-            title: Container(
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    _getCloseButton(),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(
-                          20, (style.isCloseButton ? 0 : 20), 20, 0),
-                      child: Column(
-                        children: <Widget>[
-                          _getImage(),
-                          SizedBox(
-                            height: 15,
-                          ),
-                          Text(
-                            title,
-                            style: style.titleStyle,
-                            textAlign: TextAlign.center,
-                          ),
-                          SizedBox(
-                            height: desc == null ? 5 : 10,
-                          ),
-                          desc == null
-                              ? Container()
-                              : Text(
-                                  desc,
-                                  style: style.descStyle,
-                                  textAlign: TextAlign.center,
-                                ),
-                          content == null ? Container() : content,
-                        ],
-                      ),
-                    )
-                  ],
+          child: SingleChildScrollView(
+            child: AlertDialog(
+              backgroundColor: style.backgroundColor ?? Theme.of(context).dialogBackgroundColor,
+              shape: style.alertBorder ?? _defaultShape(),
+              titlePadding: EdgeInsets.all(0.0),
+              title: Container(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      _getCloseButton(),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(
+                            20, (style.isCloseButton ? 0 : 20), 20, 0),
+                        child: Column(
+                          children: <Widget>[
+                            _getImage(),
+                            SizedBox(
+                              height: 15,
+                            ),
+                            Text(
+                              title,
+                              style: style.titleStyle,
+                              textAlign: TextAlign.center,
+                            ),
+                            SizedBox(
+                              height: desc == null ? 5 : 10,
+                            ),
+                            desc == null
+                                ? Container()
+                                : Text(
+                                    desc,
+                                    style: style.descStyle,
+                                    textAlign: TextAlign.center,
+                                  ),
+                            content == null ? Container() : content,
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
-            ),
-            contentPadding: style.buttonAreaPadding,
-            content: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: _getButtons(),
+              contentPadding: style.buttonAreaPadding,
+              content: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: _getButtons(),
+              ),
             ),
           ),
         ),
